ef83cf843933eaf0e61fd8a5c3d32c07aee9b62c
Seunghwan Choi <dadadak4@gmail.com>
2019-11-25 06:50:43 +0900

    Add train()

--- config.py
+++ config.py
@@ -8,18 +8,17 @@ config.dataset_dir = './datasets/'
 config.tensorboard_dir = './tensorboard/'
 config.checkpoint_dir = './checkpoints/'
 
-config.batch_size = 0
+config.batch_size = 128
 config.noise_features = 96
-config.num_workers = 0
+config.num_workers = 4
 
-config.lr = 0
-config.momentum = 0
-config.weight_decay = 0
+config.gen_lr = 0.001
+config.dis_lr = 0.001
+config.betas = (0.5, 0.999)
 
-config.print_step = 0
-config.tensorboard_step = 0
-config.checkpoint_step = 0
-config.load_iter = 0
-config.train_iters = 0
+config.print_step = 100
+config.tensorboard_step = 100
+config.load_epoch = 0
+config.train_epochs = 10
 config.is_train = True
 config.use_cuda = True
--- main.py
+++ main.py
@@ -1,9 +1,84 @@
 import os
 
 import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
 
 from config import config
+from models import GAN
+
+
+def train(models, writer, device):
+    train_dataset = datasets.MNIST(config.dataset_dir, train=True, transform=transforms.ToTensor(), download=True)
+    val_dataset = datasets.MNIST(config.dataset_dir, train=True, transform=transforms.ToTensor())
+
+    indices = list(range(len(train_dataset)))
+    train_sampler = SubsetRandomSampler(indices[:55000])
+    val_sampler = SubsetRandomSampler(indices[55000:])
+
+    train_loader = DataLoader(train_dataset, batch_size=config.batch_size,
+                              sampler=train_sampler, num_workers=config.num_workers)
+    val_loader = DataLoader(val_dataset, batch_size=config.batch_size,
+                            sampler=val_sampler, num_workers=config.num_workers)
+
+    criterion = nn.BCEWithLogitsLoss()
+    gen_optimizer = optim.Adam(models.gen.parameters(), lr=config.gen_lr, betas=config.betas)
+    dis_optimizer = optim.Adam(models.dis.parameters(), lr=config.dis_lr, betas=config.betas)
+
+    idx = 0
+    real_label = torch.ones(1, device=device)
+    fake_label = torch.zeros(1, device=device)
+    for _ in range(config.train_epochs):
+        for data, _ in train_loader:
+            data = data.view(-1, 784).to(device)
+
+            # ----------------------------------------------------------------------------------------------------------
+            #                                          Train the discriminator
+            # ----------------------------------------------------------------------------------------------------------
+            noise = torch.rand((data.size(0), config.noise_features), device=device) * 2 - 1
+            generated = models.gen(noise)
+
+            data_logits = models.dis(data)
+            generated_logits = models.dis(generated.detach())
+
+            real_labels = real_label.expand_as(data_logits)
+            fake_labels = fake_label.expand_as(data_logits)
+            dis_loss = criterion(data_logits, real_labels) + criterion(generated_logits, fake_labels)
+
+            dis_optimizer.zero_grad()
+            dis_loss.backward()
+            dis_optimizer.step()
+
+            # ----------------------------------------------------------------------------------------------------------
+            #                                            Train the generator
+            # ----------------------------------------------------------------------------------------------------------
+            noise = torch.rand((data.size(0), config.noise_features), device=device) * 2 - 1
+            generated = models.gen(noise)
+
+            generated_logits = models.dis(generated)
+            gen_loss = criterion(generated_logits, real_labels)
+
+            gen_optimizer.zero_grad()
+            gen_loss.backward()
+            gen_optimizer.step()
+
+            if (idx + 1) % config.print_step == 0:
+                print("Iteration: {}/{}, gen_loss: {:.4f}, dis_loss: {:.4f}".format(
+                    idx + 1, (55000 // config.batch_size + 1) * config.train_epochs, gen_loss.item(), dis_loss.item()))
+
+            if (idx + 1) % config.tensorboard_step == 0:
+                writer.add_scalar('Loss/generator', gen_loss.item(), idx + 1)
+                writer.add_scalar('Loss/discriminator', dis_loss.item(), idx + 1)
+
+                data = data.view(-1, 1, 28, 28)
+                generated = generated.view(-1, 1, 28, 28)
+                writer.add_images('Result/data', data, idx + 1)
+                writer.add_images('Result/generated', generated, idx + 1)
+
+            idx += 1
 
 
 def main():
@@ -13,13 +88,16 @@ def main():
         os.makedirs(os.path.join(config.checkpoint_dir, config.name))
 
     device = torch.device('cuda:0' if config.use_cuda else 'cpu')
-    if config.load_iter != 0:
+    models = GAN(config).to(device)
+    if config.load_epoch != 0:
         pass
 
     if config.is_train:
+        models.train()
         writer = SummaryWriter(log_dir=os.path.join(config.tensorboard_dir, config.name))
+        train(models, writer, device)
     else:
-        pass
+        models.eval()
 
 
 if __name__ == '__main__':
